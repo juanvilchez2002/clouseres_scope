569564d56d483b30144807c31635bc64747d0256
Juan Vilchez <juanvilchez2002@gmail.com>
2021-04-29 00:18:11 -0500

    Local Scope

---
+++ src/scope/local.js
@@ -0,0 +1,26 @@
+const helloWolrd = () => {
+    const hello = 'Hello World';
+    console.log(hello);
+}
+
+helloWolrd();
+
+//console.log(hello); // aqui no se puede acceder a hello x ser local
+
+//creamos una variable global
+var scope = 'soy global';
+
+const functionScope = () => {
+    //creamos una variable con el mismo nombre de la variable local
+    //pero en este caso se crea una variable local pues esta no esta reasignada
+    var scope = 'soy local'
+    const func = () => {
+        return scope
+    }
+
+    console.log(func());
+}
+
+functionScope();//imprime el valor de la variable local
+console.log(scope);//imprime el valor de la variable global
+
