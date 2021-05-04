d98baa0d3d5d75ce170f23d0e48523568489be2c
Juan Vilchez <juanvilchez2002@gmail.com>
2021-05-03 19:21:10 -0500

    Ambito Lexico en closures

---
+++ src/closure/lexical.js
@@ -0,0 +1,20 @@
+//creando un closures
+const buildCount = (i) => {
+    let count = i;
+
+    const displayCount = () => {
+        console.log(count++);
+    }
+
+    return displayCount;
+}
+
+const myCount = buildCount(1);
+myCount();
+myCount();
+myCount();
+myCount();
+
+const myOtherCount = buildCount(10);
+myOtherCount();
+myOtherCount();
